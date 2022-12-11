9ff4cd5bbc5032b249d3792a2cd2cb304d5473cf
presentKey <kim.hyeonjae4@gmail.com>
2022-12-11 23:26:45 +0900

    new: Create TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,11 @@
+import styles from './TodoList.module.css';
+
+function TodoList() {
+  return (
+    <div className={styles.todoList}>
+      <section className={styles.todoListSection}></section>
+    </div>
+  );
+}
+
+export default TodoList;
