4f81a70609e4cfe41a75ad559f59d8b8809f87ed
presentKey <kim.hyeonjae4@gmail.com>
2022-12-12 02:06:58 +0900

    style: Create Header component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,12 @@
+import Header from './Header';
 import styles from './TodoList.module.css';
 
 function TodoList() {
   return (
     <div className={styles.todoList}>
-      <section className={styles.todoListSection}></section>
+      <section className={styles.todoListSection}>
+        <Header />
+      </section>
     </div>
   );
 }
