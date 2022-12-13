ec1b729eaec9e02d211d271bd86a9be546bf9af8
presentKey <kim.hyeonjae4@gmail.com>
2022-12-13 21:48:44 +0900

    remove: Remove unused files

--- src/components/StateButton.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './StateButton.module.css';
-
-export default function StateButton({ text }) {
-  return <button className={styles.stateBtn}>{text}</button>;
-}
