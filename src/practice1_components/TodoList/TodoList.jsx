c4fffceb024275ff38c891dae09552b5abb7c280
presentKey <kim.hyeonjae4@gmail.com>
2022-12-17 00:30:00 +0900

    new: Implement delete item and update status

--- src/practice1_components/TodoList/TodoList.jsx
+++ src/practice1_components/TodoList/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Addtodo from '../AddTodo/Addtodo';
+import Todo from '../Todo/Todo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
@@ -7,11 +8,21 @@ export default function TodoList() {
     { id: '2', text: '공부하기', status: 'active' },
   ]);
   const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
+  const handleDelete = (deleted) => {
+    setTodos(todos.filter((todo) => todo.id !== deleted.id));
+  };
   return (
     <section>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
       <Addtodo onAdd={handleAdd} />
