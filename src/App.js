9a9eb53dd907bbb831bc142f6f86bb05110590cf
presentKey <kim.hyeonjae4@gmail.com>
2022-12-23 21:02:01 +0900

    new: Implement dark mode context

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import TodoList from './practice1_components/TodoList/TodoList';
 import Header from './practice1_components/Header/Header';
 import { useState } from 'react';
+import { DarkModeProvider } from './practice1_components/context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed'];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header filters={filters} filter={filter} onFilterChange={setFilter} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
--- src/practice1_components/Header/Header.jsx
+++ src/practice1_components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDarkMode } from '../context/DarkModeContext';
+import { HiMoon, HiSun } from 'react-icons/hi';
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
-    <ul>
-      {filters.map((value, index) => (
-        <li key={index}>
-          <button onClick={() => onFilterChange(value)}>{value}</button>
-        </li>
-      ))}
-    </ul>
+    <header>
+      <button onClick={toggleDarkMode}>
+        {!darkMode && <HiMoon />}
+        {darkMode && <HiSun />}
+      </button>
+      <ul>
+        {filters.map((value, index) => (
+          <li key={index}>
+            <button onClick={() => onFilterChange(value)}>{value}</button>
+          </li>
+        ))}
+      </ul>
+    </header>
   );
 }
