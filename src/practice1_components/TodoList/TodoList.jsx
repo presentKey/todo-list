80b5a98aab792046c825968c09b8ddb438df1abf
presentKey <kim.hyeonjae4@gmail.com>
2022-12-23 21:13:02 +0900

    new: Save todos to local storage

--- src/practice1_components/TodoList/TodoList.jsx
+++ src/practice1_components/TodoList/TodoList.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Addtodo from '../AddTodo/Addtodo';
 import Todo from '../Todo/Todo';
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: '1', text: '운동하기', status: 'active' },
-    { id: '2', text: '공부하기', status: 'active' },
-  ]);
+  const [todos, setTodos] = useState(readTodosFromLocalStroage);
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) =>
     setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
-  const handleDelete = (deleted) => {
+  const handleDelete = (deleted) =>
     setTodos(todos.filter((todo) => todo.id !== deleted.id));
-  };
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   const filtered = getFilteredItems(todos, filter);
   return (
     <section>
@@ -29,6 +28,10 @@ export default function TodoList({ filter }) {
       <Addtodo onAdd={handleAdd} />
     </section>
   );
+}
+function readTodosFromLocalStroage() {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
 }
 
 function getFilteredItems(todos, filter) {
